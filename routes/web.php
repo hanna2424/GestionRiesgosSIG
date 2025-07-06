f083bea05a1ef508bd31d732f8b4e01621985c32
Vintailsz1 <qbryan303@gmail.com>
2025-07-06 12:32:50 -0500

    xd

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\GestionController;
+use App\Http\Controllers\ZonaRiesgoController;
 
 Route::get('/', [LoginController::class, 'index']);
 Route::get('/menu', [GestionController::class, 'index']);
@@ -11,5 +12,7 @@ Route::get('/listado', [GestionController::class, 'listado']);
 
 Route::get('/logout', [LoginController::class, 'logout']);
 
+
 Route::resource('login', LoginController::class);
 Route::resource('gestion', GestionController::class);
+Route::resource('riesgos', ZonaRiesgoController::class);
