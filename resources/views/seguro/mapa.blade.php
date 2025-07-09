b673dd299a0c9834d904a7dc7bbb9d2eb6b01ad9
hanna2424 <armijosalejandra2001@gmail.com>
2025-07-09 14:59:29 -0500

    creando mapa y uniendo con la plantilla

--- resources/views/seguro/mapa.blade.php
+++ resources/views/seguro/mapa.blade.php
@@ -6,7 +6,32 @@ Ver Zonas de Riesgo
 
 @section('contenido')
 <br>
-<h1>Mapa de Clientes</h1>
+<h1>Mapa de Zonas Seguras</h1>
 <br>
+<div id="mapa-seguro" style="border:2px solid black;height:500px;width:100%;"></div>
 
+<script type="text/javascript">
+
+      function initMap(){
+        //alert("mapa ok");
+        var latitud_longitud= new google.maps.LatLng(-0.9374805,-78.6161327);
+        var mapa=new google.maps.Map(
+          document.getElementById('mapa-seguro'),
+          {
+            center:latitud_longitud,
+            zoom:7,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+          }
+        );  
+        @foreach($riesgos as $mr)
+            var coordenadaCliente= new google.maps.LatLng({{$mr->latitud}},{{$mr->longitud}});
+            var marcador=new google.maps.Marker({
+                position:coordenadaCliente,
+                map:mapa,
+                title:"{{$mr->nombre}} {{$mr->seguridad}}",
+                draggable:false
+            });   
+        @endforeach
+      }
+</script>   
 @endsection
