8ce26b340bfea5e3d49c5b32945e6af19bd4d675
Vintailsz1 <qbryan303@gmail.com>
2025-07-09 21:20:54 -0500

    Intentando agregar login por roles y registro de usuarios

---
+++ app/Http/Controllers/RegistroController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+
+class RegistroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+
+        if (Session::has('usuario')) {
+            Session::forget('usuario');
+            return redirect('/')->with('info', 'Hay una sesion abierta....cerrando...');
+        }
+
+        else{
+            return view('registro.registro');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+
+        $usuario = new Usuario();
+        $usuario->username = $request->username;
+        $usuario->password = Hash::make($request->password);
+        $usuario->email = $request->email;
+        $usuario->rol = 'visitante'; 
+        $usuario->save();
+
+        return redirect('/')->with('success', 'Registro exitoso.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
